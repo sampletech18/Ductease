99da7de02a255133f5416bcecd206ea018e3af5b
sampletech18 <workproject947@gmail.com>
2025-07-27 08:41:27 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+class Config:
+    SECRET_KEY = "supersecretkey"
+    SQLALCHEMY_DATABASE_URI = "postgresql://duct_db_user:SXQ9iAKpluAXibt4xcxhakJk4uoQCFko@dpg-d2075pp5pdvs73c6q740-a.singapore-postgres.render.com/duct_db"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
