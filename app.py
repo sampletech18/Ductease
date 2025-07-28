f917d6e903df2efe1d0477b3f901badab36c3ffe
sampletech18 <workproject947@gmail.com>
2025-07-28 08:47:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, session, j
 from flask_sqlalchemy import SQLAlchemy
 import os
 from datetime import datetime
+from sqlalchemy import text
 
 
 app = Flask(__name__)
