4cba4f8719ed93c6f0a9c31c93ce90062da042ba
sampletech18 <workproject947@gmail.com>
2025-07-28 08:35:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, redirect, url_for, session, j
 from flask_sqlalchemy import SQLAlchemy
 import os
 from datetime import datetime
+from models import db, User, Vendor  # adjust based on your structure
+
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
